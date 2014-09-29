da22ffb8cd42354194ea0c4a88b995d50d01f911
Chris Fries <cfries@motorola.com>
2014-09-28 22:58:09 -0500

    Staging: android: binder: Ratelimit binder debug messages
    Ratelimit the binder debug messages, since they can get spammy and
    flood the entire kernel log.
    
    In some cases, enabling serial console with a spammy binder error can
    cause a watchdog panic (and we don't have reports of this happening
    with serial console disabled).
    
    Bug: 17613664
    Change-Id: Iecdb4c3c80ccf00c43459e93c17f5369fd55e6e7
    Signed-off-by: Chris Fries <cfries@motorola.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -140,7 +140,7 @@ module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 #define binder_debug(mask, x...) \
 	do { \
 		if (binder_debug_mask & mask) \
-			pr_info(x); \
+			pr_info_ratelimited(x); \
 	} while (0)
 
 #define binder_user_error(x...) \
