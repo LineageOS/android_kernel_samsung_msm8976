9f6db7d70090944750708e17adaf16aad5a96bec
Archana Ramachandran <archanar@qca.qualcomm.com>
2016-01-25 16:57:02 -0800

    cld-2.0: Fix mode switch to 1x1 not updating HT SMPS in the reassoc request
    Dynamic Antenna mode switch from 2x2 to 1x1 in the non connected state
    is updating the association request HT SMPS to static but the reassociation
    request HT SMPS mode is still disabled. This will lead to station
    reassociating in 2x2 mode although the current antenna mode is 1x1.
    
    Change-Id: Iafbaf7f97ce56171b3fde469550a11ebfa20b0fc
    CRs-Fixed: 966939

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limFT.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limFT.c
@@ -1023,6 +1023,11 @@ void limFillFTSession(tpAniSirGlobal pMac,
    pftSessionEntry->htRecommendedTxWidthSet =
       pftSessionEntry->htSupportedChannelWidthSet;
 
+   pftSessionEntry->enableHtSmps = psessionEntry->enableHtSmps;
+   pftSessionEntry->smpsMode = psessionEntry->smpsMode;
+   limLog(pMac, LOG1, FL("FT session enable smps: %d mode: %d"),
+          pftSessionEntry->enableHtSmps, pftSessionEntry->smpsMode);
+
    vos_mem_free(pBeaconStruct);
 }
 
