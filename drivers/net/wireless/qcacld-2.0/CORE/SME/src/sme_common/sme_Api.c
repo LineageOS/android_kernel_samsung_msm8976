61a3b4a5c7a865bf8d1a6aefe38c6fd86208bff0
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2016-04-27 20:03:38 +0530

    qcacld-2.0: Resolve memory leak issues
    prima to qcacld-2.0 propagation
    
    In SME, if command posting fails then memory allocated for
    command is not freed.
    Free the memory if SME fails to post command.
    
    Change-Id: I281ef5eb9492fe75d639b2bef7ed588aacee8e74
    CRs-Fixed: 974567

--- drivers/net/wireless/qcacld-2.0/CORE/SME/src/sme_common/sme_Api.c
+++ drivers/net/wireless/qcacld-2.0/CORE/SME/src/sme_common/sme_Api.c
@@ -2487,6 +2487,7 @@ eHalStatus sme_SetEseBeaconRequest(tHalHandle hHal, const tANI_U8 sessionId,
    if(status != eHAL_STATUS_SUCCESS)
       pSmeRrmContext->eseBcnReqInProgress = FALSE;
 
+   vos_mem_free(pSmeBcnReportReq);
    return status;
 }
 
@@ -7356,6 +7357,7 @@ eHalStatus smeIssueFastRoamNeighborAPEvent(tHalHandle hHal,
              pUsrCtx = vos_mem_malloc(sizeof(*pUsrCtx));
              if (NULL == pUsrCtx) {
                  smsLog(pMac, LOGE, FL("Memory allocation failed"));
+                 sme_ReleaseGlobalLock( &pMac->sme );
                  return eHAL_STATUS_FAILED_ALLOC;
              }
 
@@ -12185,6 +12187,7 @@ eHalStatus sme_get_rssi(tHalHandle hal, struct sir_rssi_req req,
 		if (!VOS_IS_STATUS_SUCCESS(vosstatus)) {
 			VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
 				"%s: Post get rssi msg fail", __func__);
+			vos_mem_free(vosmessage.bodyptr);
 			status = eHAL_STATUS_FAILURE;
 		}
 		sme_ReleaseGlobalLock(&mac->sme);
@@ -14139,6 +14142,12 @@ eHalStatus sme_SendRateUpdateInd(tHalHandle hHal,
 
         sme_ReleaseGlobalLock(&pMac->sme);
         return eHAL_STATUS_SUCCESS;
+    }
+    else
+    {
+        vos_mem_free(rateUpdate);
+        VOS_TRACE( VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
+                   FL("sme_AcquireGlobalLock error"));
     }
 
     return status;
