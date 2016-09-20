6346c30c8e6b91d946cd6675935c375fbd01044b
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-09-19 18:14:49 -0700

    qcacld-2.0: Validate multicast filter count
    Before configuring to firmware, validate multicast
    filter count does not exceed the max allowed value.
    
    Change-Id: I1bcb1c820a5154109565a8c283bf5131a3b90855
    CRs-Fixed: 1059974
    Bug: 31037507
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -23663,8 +23663,11 @@ static VOS_STATUS wma_process_mcbc_set_filter_req(tp_wma_handle wma_handle,
 	uint8_t vdev_id = 0;
 	int i;
 
-	if(mcbc_param->ulMulticastAddrCnt <= 0) {
-		WMA_LOGW("Number of multicast addresses is 0");
+	if (mcbc_param->ulMulticastAddrCnt <= 0 ||
+		mcbc_param->ulMulticastAddrCnt >
+			CFG_TGT_MAX_MULTICAST_FILTER_ENTRIES) {
+		WMA_LOGW("Number of multicast addresses is: %u",
+				mcbc_param->ulMulticastAddrCnt);
 		return VOS_STATUS_E_FAILURE;
 	}
 
