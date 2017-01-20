7a18b321713d5136e24a8253cf52668fed3ad185
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-20 14:24:23 +0100

    drivers: firmware: Add firmware-modem fw path

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -298,7 +298,8 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware",
-	"/firmware/image"
+	"/firmware/image",
+	"/firmware-modem/image"
 };
 
 /*
