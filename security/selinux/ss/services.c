7432838118e64458e5bc0d2ddf0418322885a70a
Luca Stefani <luca020400@lineageos.org>
2017-04-18 12:51:52 +0200

    security: selinux: Partially remove samsung changes
    Change-Id: I6dddda580ab07d5b1a7f408a538349d27ecf0569

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -772,11 +772,6 @@ out:
 	kfree(n);
 	kfree(t);
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EPERM;
@@ -1535,11 +1530,6 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EACCES;
@@ -1831,11 +1821,6 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	char *s;
 	u32 len;
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (selinux_enforcing)
 		return -EINVAL;
 
@@ -2526,7 +2511,6 @@ int security_fs_use(
 {
 	int rc = 0;
 	struct ocontext *c;
-	u32 tmpsid;
 
 	read_lock(&policy_rwlock);
 
@@ -2541,8 +2525,7 @@ int security_fs_use(
 		*behavior = c->v.behavior;
 		if (!c->sid[0]) {
 			rc = sidtab_context_to_sid(&sidtab, &c->context[0],
-						   &tmpsid);
-			c->sid[0] = tmpsid;
+						   &c->sid[0]);
 			if (rc)
 				goto out;
 		}
