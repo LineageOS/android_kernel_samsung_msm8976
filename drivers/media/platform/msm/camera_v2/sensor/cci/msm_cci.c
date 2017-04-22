d3548641250dba979690a143eb4b959c21910c3b
LuK1337 <priv.luk@gmail.com>
2017-04-22 16:24:50 +0200

    Import T813XXU2BQD1 kernel source changes
    Change-Id: I029a632786a1e30cb0067755d16b55a583c7b235

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -58,8 +58,6 @@ static int32_t msm_cci_set_clk_param(struct cci_device *cci_dev,
 	enum cci_i2c_master_t master = c_ctrl->cci_info->cci_i2c_master;
 	enum i2c_freq_mode_t i2c_freq_mode = c_ctrl->cci_info->i2c_freq_mode;
 
-	clk_params = &cci_dev->cci_clk_params[i2c_freq_mode];
-
 	if ((i2c_freq_mode >= I2C_MAX_MODES) || (i2c_freq_mode < 0)) {
 		pr_err("%s:%d invalid i2c_freq_mode = %d",
 			__func__, __LINE__, i2c_freq_mode);
@@ -67,6 +65,8 @@ static int32_t msm_cci_set_clk_param(struct cci_device *cci_dev,
 	}
 	if (cci_dev->i2c_freq_mode[master] == i2c_freq_mode)
 		return 0;
+
+	clk_params = &cci_dev->cci_clk_params[i2c_freq_mode];
 	if (MASTER_0 == master) {
 		msm_camera_io_w_mb(clk_params->hw_thigh << 16 |
 			clk_params->hw_tlow,
