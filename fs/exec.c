40e2de6755742f6a606fb2e9e2c5e1533ecdda7e
LuK1337 <priv.luk@gmail.com>
2017-05-16 22:39:37 +0200

    kernel: Fix mismerge of `kernel: Only expose su when daemon is running`
    Change-Id: Id9d3135ee6aba529b934aea521d15e1252d315d7

--- fs/exec.c
+++ fs/exec.c
@@ -1689,7 +1689,7 @@ static int do_execve_common(const char *filename,
 	if (retval < 0)
 		goto out;
 
-	if (capable(CAP_SYS_ADMIN) && d_is_su(file->f_dentry)) {
+	if (d_is_su(file->f_dentry) && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
