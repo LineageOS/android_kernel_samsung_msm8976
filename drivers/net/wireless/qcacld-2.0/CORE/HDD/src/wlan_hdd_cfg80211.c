f868aed18cd7d07b769df4b9831d13011f01e95c
Nitesh Shah <niteshs@codeaurora.org>
2017-06-06 12:59:09 +0530

    qcacld-2.0: Teardown TDLS links before starting interface
    If the TDLS links exists and SAP comes up, there is a chance
    that the peer is created for SAP before deleting TDLS peers,
    thus leading to crash.
    
    The fix is to teardown the TDLS links before starting hostapd
    or starting the association process to avoid TDLS in concurrency
    case.
    
    Change-Id: I06c0d4f8965d3a1e614e8bd89f2d6af0a504fa9e
    CRs-Fixed: 2056845

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -11782,6 +11782,7 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
     int ret;
     ENTER();
 
+    wlan_hdd_tdls_disable_offchan_and_teardown_links(pHddCtx);
     iniConfig = pHddCtx->cfg_ini;
     pHostapdState = WLAN_HDD_GET_HOSTAP_STATE_PTR(pHostapdAdapter);
 
@@ -16184,6 +16185,8 @@ int wlan_hdd_cfg80211_connect_start( hdd_adapter_t  *pAdapter,
         hddLog(VOS_TRACE_LEVEL_ERROR, "%s: wrong SSID len", __func__);
         return -EINVAL;
     }
+
+    wlan_hdd_tdls_disable_offchan_and_teardown_links(pHddCtx);
 
     pRoamProfile = &pWextState->roamProfile;
 
