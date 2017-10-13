a1781200066e3ccd2e68247c3edcfc4026d09a6c
Zhen Kong <zkong@codeaurora.org>
2017-10-13 14:01:48 -0700

    qseecom: fix an app unloading issue when the app is blocked
    Reset app_crash flag to make sure app entry is not freed when
    there is another client still being blocked on the same app.
    
    Change-Id: I25d236abc97e60fe8a4abbbc8c086291c764a9c1
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -2270,6 +2270,8 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 					(void *)data->client.app_name,
 					strlen(data->client.app_name))) {
 					found_app = true;
+					if (ptr_app->app_blocked)
+						app_crash = false;
 					if (app_crash || ptr_app->ref_cnt == 1)
 						unload = true;
 					break;
