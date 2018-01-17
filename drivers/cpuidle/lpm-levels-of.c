4be5f1ef2bf497d645d9ae753ef708579998733a
syphyr <syphyr@gmail.com>
2018-01-18 00:47:18 +0100

    cpuidle: Remove unnecessary WARN for calculate_residency
    WARN:
    calculate_residency: Incorrect power attributes for LPM
    
    Change-Id: I186bcd8d8dee5a0c1961770498749c7ef9df7b74

--- drivers/cpuidle/lpm-levels-of.c
+++ drivers/cpuidle/lpm-levels-of.c
@@ -890,7 +890,7 @@ static int calculate_residency(struct power_params *base_pwr,
 	residency /= (int32_t)(base_pwr->ss_power  - next_pwr->ss_power);
 
 	if (residency < 0) {
-		__WARN_printf("%s: Incorrect power attributes for LPM\n",
+		pr_err("%s: Incorrect power attributes for LPM\n",
 				__func__);
 		return 0;
 	}
