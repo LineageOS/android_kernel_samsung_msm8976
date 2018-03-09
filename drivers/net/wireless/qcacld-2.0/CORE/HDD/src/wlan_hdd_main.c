738f177885cf9e5121aaac5b31955f4720de93d2
Huashan Qu <hqu@codeaurora.org>
2018-03-09 21:59:33 +0800

    qcacld-2.0: Introduce and enable HDD Request Manager infrastructure
    propagation from qcacld-3.0 to qcacld-2.0
    
    List qcacld-3.0 changes as following:
    "Change-Id: I4e598e51983475318bc668e786aca690a934bd6c",
    "Change-Id: I31e268ca02b4b5c2831c540933ee059a27bd9c7e",
    "Change-Id: If4d5912710f8a3b5e87adf76f828a646b7cc2983".
    
    Many operations within the wlan driver occur in an asynchronous
    manner. Requests are received by HDD via one of the kernel interfaces
    (ioctl, nl80211, virtual file system, etc.). The requests are
    translated to an internal format and are then passed to lower layers
    for processing. For requests which require a response, that response
    comes up from the lower layers in a separate thread of execution,
    ultimately resulting in a call to a callback function that was
    provided by HDD as part of the initial request. So a mechanism is
    needed to synchronize the request and response.
    
    Currently there are various mechanisms which perform these
    synchronizations, but experience with them has revealed some flaws.
    So an universal mechanism is needed to synchronize the request and
    response which addresses all of the known flaws. This framework
    provides that mechanism. Enable the HDD Request Manager by invoking
    the init() and deinit() APIs as appropriate.
    
    Change-Id: Ic4267507dcdbe550d49422bf3e75450ba66021aa
    CRs-Fixed: 2205626

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
@@ -77,6 +77,7 @@
 #include <wlan_hdd_wowl.h>
 #include <wlan_hdd_misc.h>
 #include <wlan_hdd_wext.h>
+#include "wlan_hdd_request_manager.h"
 #include "wlan_hdd_trace.h"
 #include "vos_types.h"
 #include "vos_trace.h"
@@ -13522,6 +13523,8 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
 
    hdd_wlan_green_ap_deinit(pHddCtx);
 
+   hdd_request_manager_deinit();
+
    //Close Watchdog
    if (pConfig && pConfig->fIsLogpEnabled)
       vos_watchdog_close(pVosContext);
@@ -14904,6 +14907,7 @@ int hdd_wlan_startup(struct device *dev, v_VOID_t *hif_sc)
       goto err_wdclose;
    }
 
+   hdd_request_manager_init();
    hdd_wlan_green_ap_init(pHddCtx);
 
    status = vos_open( &pVosContext, 0);
@@ -15676,6 +15680,7 @@ err_vos_nv_close:
    vos_nv_close();
 
    hdd_wlan_green_ap_deinit(pHddCtx);
+   hdd_request_manager_deinit();
 
 err_wdclose:
    if(pHddCtx->cfg_ini->fIsLogpEnabled)
