b6009f97fc3468436c0cd76a7e38003d510adf49
syphyr <syphyr@gmail.com>
2018-04-13 21:31:36 +0200

    Revert "qcacld-2.0: Race condition while using pkt log buffer"
    This reverts commit 43abf687b82b6ed625fb27275187c31d82dfea70.

--- drivers/net/wireless/qcacld-2.0/CORE/UTILS/PKTLOG/linux_ac.c
+++ drivers/net/wireless/qcacld-2.0/CORE/UTILS/PKTLOG/linux_ac.c
@@ -546,12 +546,10 @@ static void pktlog_detach(struct ol_softc *scn)
 	pl_info = pl_dev->pl_info;
 	remove_proc_entry(WLANDEV_BASENAME, g_pktlog_pde);
 	pktlog_sysctl_unregister(pl_dev);
+	pktlog_cleanup(pl_info);
 
-	spin_lock_bh(&pl_info->log_lock);
 	if (pl_info->buf)
 		pktlog_release_buf(scn);
-	spin_unlock_bh(&pl_info->log_lock);
-	pktlog_cleanup(pl_info);
 
 	if (pl_dev) {
 		vos_mem_free(pl_info);
@@ -601,16 +599,12 @@ pktlog_read_proc_entry(char *buf, size_t nbytes, loff_t *ppos,
 	int rem_len;
 	int start_offset, end_offset;
 	int fold_offset, ppos_data, cur_rd_offset, cur_wr_offset;
-	struct ath_pktlog_buf *log_buf;
-
-	spin_lock_bh(&pl_info->log_lock);
-	log_buf = pl_info->buf;
+	struct ath_pktlog_buf *log_buf = pl_info->buf;
 
 	*read_complete = false;
 
 	if (log_buf == NULL) {
 		*read_complete = true;
-		spin_unlock_bh(&pl_info->log_lock);
 		return 0;
 	}
 
@@ -713,6 +707,7 @@ rd_done:
 	*ppos += ret_val;
 
 	if (ret_val == 0) {
+		PKTLOG_LOCK(pl_info);
 		/* Write pointer might have been updated during the read.
 		 * So, if some data is written into, lets not reset the pointers.
 		 * We can continue to read from the offset position
@@ -726,8 +721,9 @@ rd_done:
 			pl_info->buf->offset = PKTLOG_READ_OFFSET;
 			*read_complete = true;
 		}
+		PKTLOG_UNLOCK(pl_info);
 	}
-	spin_unlock_bh(&pl_info->log_lock);
+
 	return ret_val;
 }
 
@@ -747,15 +743,10 @@ pktlog_read(struct file *file, char *buf, size_t nbytes, loff_t *ppos)
 	struct ath_pktlog_info *pl_info = (struct ath_pktlog_info *)
 					  proc_entry->data;
 #endif
-	struct ath_pktlog_buf *log_buf;
+	struct ath_pktlog_buf *log_buf = pl_info->buf;
 
-	spin_lock_bh(&pl_info->log_lock);
-	log_buf = pl_info->buf;
-
-	if (log_buf == NULL) {
-		spin_unlock_bh(&pl_info->log_lock);
+	if (log_buf == NULL)
 		return 0;
-	}
 
 	if (*ppos == 0 && pl_info->log_state) {
 		pl_info->saved_state = pl_info->log_state;
@@ -770,13 +761,11 @@ pktlog_read(struct file *file, char *buf, size_t nbytes, loff_t *ppos)
 
 	if (*ppos < bufhdr_size) {
 		count = MIN((bufhdr_size - *ppos), rem_len);
-		spin_unlock_bh(&pl_info->log_lock);
 		if (copy_to_user(buf, ((char *)&log_buf->bufhdr) + *ppos,
 				 count))
 			return -EFAULT;
 		rem_len -= count;
 		ret_val += count;
-		spin_lock_bh(&pl_info->log_lock);
 	}
 
 	start_offset = log_buf->rd_offset;
@@ -818,25 +807,21 @@ pktlog_read(struct file *file, char *buf, size_t nbytes, loff_t *ppos)
 			goto rd_done;
 
 		count = MIN(rem_len, (end_offset - ppos_data + 1));
-		spin_unlock_bh(&pl_info->log_lock);
 		if (copy_to_user(buf + ret_val,
 				 log_buf->log_data + ppos_data,
 				 count))
 			return -EFAULT;
 		ret_val += count;
 		rem_len -= count;
-		spin_lock_bh(&pl_info->log_lock);
 	} else {
 		if (ppos_data <= fold_offset) {
 			count = MIN(rem_len, (fold_offset - ppos_data + 1));
-			spin_unlock_bh(&pl_info->log_lock);
 			if (copy_to_user(buf + ret_val,
 					 log_buf->log_data + ppos_data,
 					 count))
 				return -EFAULT;
 			ret_val += count;
 			rem_len -= count;
-			spin_lock_bh(&pl_info->log_lock);
 		}
 
 		if (rem_len == 0)
@@ -848,14 +833,12 @@ pktlog_read(struct file *file, char *buf, size_t nbytes, loff_t *ppos)
 
 		if (ppos_data <= end_offset) {
 			count = MIN(rem_len, (end_offset - ppos_data + 1));
-			spin_unlock_bh(&pl_info->log_lock);
 			if (copy_to_user(buf + ret_val,
 					 log_buf->log_data + ppos_data,
 					 count))
 				return -EFAULT;
 			ret_val += count;
 			rem_len -= count;
-			spin_lock_bh(&pl_info->log_lock);
 		}
 	}
 
@@ -866,7 +849,6 @@ rd_done:
 	}
 	*ppos += ret_val;
 
-	spin_unlock_bh(&pl_info->log_lock);
 	return ret_val;
 }
 
