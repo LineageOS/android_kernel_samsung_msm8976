e5f554022106dcf9424facde1f9f20f6264a40a7
syphyr <syphyr@gmail.com>
2018-07-25 03:15:04 +0200

    Revert "arm/arm64: irq: warn only when affinity really breaks"
    This reverts commit 92b09f2deb6b0b2e54b3abb7cb7f92d9e0b48c68.

--- arch/arm/kernel/irq.c
+++ arch/arm/kernel/irq.c
@@ -155,7 +155,6 @@ static bool migrate_one_irq(struct irq_desc *desc)
 {
 	struct irq_data *d = irq_desc_get_irq_data(desc);
 	const struct cpumask *affinity = d->affinity;
-	bool ret = false;
 
 	/*
 	 * If this is a per-CPU interrupt, or the affinity does not
@@ -164,14 +163,10 @@ static bool migrate_one_irq(struct irq_desc *desc)
 	if (irqd_is_per_cpu(d) || !cpumask_test_cpu(smp_processor_id(), affinity))
 		return false;
 
-	if (cpumask_any_and(affinity, cpu_online_mask) >= nr_cpu_ids) {
+	if (cpumask_any_and(affinity, cpu_online_mask) >= nr_cpu_ids)
 		affinity = cpu_online_mask;
-		ret = true;
-	}
 
-	irq_set_affinity_locked(d, affinity, 0);
-
-	return ret;
+	return irq_set_affinity_locked(d, affinity, false) == IRQ_SET_MASK_OK;
 }
 
 /*
